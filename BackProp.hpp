9cad48d0a28a9ed7ae069911a336537465f3b5a7
Jac-Zac <jacopozac@icloud.com>
2021-01-31 16:25:48 +0100

    new file BackProp

---
+++ BackProp.hpp
@@ -0,0 +1,20 @@
+//
+//  BackProp.hpp
+//  AI_Tria 2.0
+//
+//  Created by Jacopo Zacchigna on 31/01/2021.
+//  Copyright © 2020 Jacopo Zacchigna. All rights reserved.
+//
+
+#pragma once
+
+#include "Net.hpp"
+
+// ********************************************* DEFINITION **********************************************
+
+class BackProp : public Net{
+public:
+	BackProp();
+protecteD:
+	void Loss();
+}
